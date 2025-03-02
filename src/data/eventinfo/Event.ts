7946bcc90b7ecd1e03b7225fadc24867c28b5a73
Waescheklammer <justin.guenther@protonmail.com>
2025-03-02 17:37:56 +0100

    changed file types

--- src/data/eventinfo/Event.ts
+++ src/data/eventinfo/Event.ts
@@ -12,6 +12,6 @@ export const events: EventInfo = {
   ticketLink: "https://nocturnaldemons.ticket.io/#",
   mapLink:
     "https://www.google.de/maps/place/NOSTROMO+(Schall+%26+Rauch+e.V.)/@51.1543368,14.9693562,453m/data=!3m2!1e3!4b1!4m6!3m5!1s0x4708dc2d5356eabb:0xe2a464ea5f708e9a!8m2!3d51.1543368!4d14.9719311!16s%2Fg%2F11bwfm6_62?entry=ttu",
-  imgPromo: "/images/logo/nostromo26.jpeg",
-  imgCover: "/images/logo/nostromo26_cover.jpeg",
+  imgPromo: "/images/logo/nostromo26.webp",
+  imgCover: "/images/logo/nostromo26_cover.webp",
 };
