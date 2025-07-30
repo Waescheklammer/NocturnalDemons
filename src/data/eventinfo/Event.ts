a1e558c3292d4c35a584ac3b4bb17abd007d0c41
Waescheklammer <justin.guenther@protonmail.com>
2025-07-30 21:04:49 +0200

    changed to webp

--- src/data/eventinfo/Event.ts
+++ src/data/eventinfo/Event.ts
@@ -12,6 +12,6 @@ export const events: EventInfo = {
   ticketLink: "https://eventfrog.de/de/p/party/electro/nocturnal-demons-apocalypse-7355667685353457674.html",
   mapLink:
     "https://www.google.de/maps/place/NOSTROMO+(Schall+%26+Rauch+e.V.)/@51.1543368,14.9693562,453m/data=!3m2!1e3!4b1!4m6!3m5!1s0x4708dc2d5356eabb:0xe2a464ea5f708e9a!8m2!3d51.1543368!4d14.9719311!16s%2Fg%2F11bwfm6_62?entry=ttu",
-  imgPromo: process.env.PUBLIC_URL + "/images/logo/apocalypse.jpeg",
-  imgCover: process.env.PUBLIC_URL + "/images/logo/apocalypse.jpeg",
+  imgPromo: process.env.PUBLIC_URL + "/images/logo/apocalypse.webp",
+  imgCover: process.env.PUBLIC_URL + "/images/logo/apocalypse.webp",
 };
